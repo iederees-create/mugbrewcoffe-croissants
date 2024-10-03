1990d8ad2cb3360119ee7d37ff89a2459bbb66c1
Iederees Francis <63100527+iederees-create@users.noreply.github.com>
2024-10-03 10:13:48 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("orderNow").addEventListener("click", function() {
+    window.location.href = "https://wa.me/+27723996099?text=Greetings!%20I%20want%20to%20place%20an%20order...";
+});
+
+document.getElementById("bookingsOpen").addEventListener("click", function() {
+    window.location.href = "https://wa.me/+27723996099?text=Greetings!%20I'd%20like%20to%20make%20a%20booking...";
+});
